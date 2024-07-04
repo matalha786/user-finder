a921f2bfcceb1d83dfd8ef9d663de6aa69f610b2
M Ahmed Talha <ahmedtalha470@gmail.com>
2024-07-04 02:08:58 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,1 @@
-from setuptools import setup, find_packages
 
-setup(
-    name="user-finder-zeta",
-    version="1.0",
-    packages=find_packages(),
-    install_requires=[
-        "rich",
-        "requests",
-        "argparse"
-    ],
-    entry_points={
-        "console_scripts": [
-            "user-finder-zeta=main:main",
-        ],
-    },
-)

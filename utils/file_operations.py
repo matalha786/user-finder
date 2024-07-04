712ee25b6c254f754e925f1fd505e5b8d370e734
M Ahmed Talha <ahmedtalha470@gmail.com>
2024-07-04 02:06:46 -0700

    Delete utils directory

--- utils/file_operations.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def isFile(filename):
-    return os.path.isfile(filename)
-
-def getLinesFromFile(filename):
-    with open(filename, "r") as file:
-        return [line.strip() for line in file.readlines()]
